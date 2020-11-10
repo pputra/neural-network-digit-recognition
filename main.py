47ba3d40b464a2c7473524b5f67eeea2a7308ad2
pputra <mynameisyoga@live.com>
2020-11-10 00:14:10 -0800

    fix: train label vector dim

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from nn_config import LAYER_SIZES, MINI_BATCH_SIZE, EPOCHS
 def get_training_data():
     train_image = genfromtxt('train_image.csv', delimiter=',', skip_footer=59900)
     train_label = genfromtxt('train_label.csv', dtype='int', delimiter=',', skip_footer=59900)
-    train_label_binary = zeros((len(train_label), 10))
-
+    train_label_binary = zeros((len(train_label), 10, 1))
     for i in range(train_label.shape[0]):
         val = train_label[i]
         train_label_binary[i][val] = 1
